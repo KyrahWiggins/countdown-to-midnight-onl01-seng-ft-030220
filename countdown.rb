d84f20af9d815e235ef55a638823e875b72c953a
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-03-06 00:52:52 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,4 +7,12 @@ def countdown(integer)
   end
   "HAPPY NEW YEAR!"
 end
-countdown(10)
+
+def countdown_with_sleep(integer)
+  while integer > 0
+  puts  "#{integer} SECOND(S)!"
+  sleep(1)
+  integer -= 1
+  end
+  "HAPPY NEW YEAR!"
+end  
